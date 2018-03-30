7a935478de3702fd5f429bc73660f0530fdd69b5
Kira Vega <kiraleigh@gmail.com>
2018-03-29 20:11:10 -0700

    Rxjs Pt 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -19,7 +19,7 @@ export class AboutComponent implements OnInit {
     private location: Location) { }
 
   ngOnInit() {
-    this.leaderservice.getLeaders().then(leaders => this.leaders =leaders);
+    this.leaderservice.getLeaders().subscribe(leaders => this.leaders =leaders);
   }
 
 }
